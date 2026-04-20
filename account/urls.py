df7fcab75d8cf2926e543e855d84371e34c43dde
Ammar Akram <ammar.akram@onenetwork.pk>
2026-04-20 17:49:17 +0500

    changes

--- account/urls.py
+++ account/urls.py
@@ -15,6 +15,5 @@ urlpatterns = [
     path('auth/me/', CurrentUserView.as_view(), name='current-user'),
     path('permissions/', PermissionListView.as_view(), name='permission-list'),
 
-
     path('', include(router.urls)),
 ]
